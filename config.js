9ce7fbe673a9eb8ab59ef6fde26ce5bfaab02da2
digilira <49098359+digilira@users.noreply.github.com>
2019-03-30 17:26:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+ 
+module.exports =  Object.freeze({
+    address: {
+        node: 'https://nodes.wavesplatform.com',
+        matcher: 'https://matcher.wavesplatform.com',
+        wallet: '...',
+    },
+    accepted: {
+        WAVES: 'WAVES',
+        BTC: '8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS',
+        ETH: '474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu',
+        TRY: '2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN',
+        LTC: 'HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk',
+    },
+    exchange: {
+        dataServices: 'https://api.wavesplatform.com/v0/pairs',
+    }, 
+});
+ 
+
+ 
